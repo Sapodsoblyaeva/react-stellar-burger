c7f72e6647d0ae4bcf0dc053ba12c0981d8231be
Sveta Podsoblyaeva <sa.podsoblyaeva@yandex.ru>
2023-11-02 20:39:27 +0300

    fix/proptypes

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,9 +1,14 @@
 
 import styles from './modal-overlay.module.css'
+import PropTypes from 'prop-types'
 
 export default function ModalOverlay({setIsOpened}) {
   return (
     <div className={styles.modalOverlay} onClick={setIsOpened}>
     </div>
   )
+}
+
+ModalOverlay.propTypes = {
+    setIsOpened: PropTypes.func
 }
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,9 +1,8 @@
 import styles from './modal.module.css'
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import PropTypes from 'prop-types'
-import { createPortal } from 'react-dom'
 import { useEffect, useCallback } from 'react'
-import { rootModal } from '../../utils/constants'
+
 
 export default function Modal({ isOpened, setIsOpened, children }) {
   const handleClose = () => {
